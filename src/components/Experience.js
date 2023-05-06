825649b483f53a78ecdb837903c9443747e80550
Manoj Vignesh K M <kmmanojv96@gmail.com>
2023-05-06 17:24:03 +0530

    timeline experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,28 +1,32 @@
-import { Container, Header, List } from "semantic-ui-react";
+import { Container, Header } from "semantic-ui-react";
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import { MdWorkOutline, MdOutlineSchool } from "react-icons/md"
 
 import "semantic-ui-css/semantic.min.css";
+import 'react-vertical-timeline-component/style.min.css';
 
 function ExperienceItem(props) {
+    let description = props.description;
+    if(props.description.indexOf(';') !== -1) {
+        description = description.split(';').map((dsc) => <li>{dsc}</li>);
+    }
     return (
-        <List.Item>
-            <List.Icon name={props.type==="education"?"graduation cap":"suitcase"} />
-            <List.Content>
-                <List.Header>{props.organization}</List.Header>
-                <List.Description>{props.role} | {props.period}</List.Description>
-                <List.List>
-                    {
-                        props.points.map((point, i) => (
-                                <List.Item key={i}>
-                                    <List.Content>
-                                        <List.Description>- {point}</List.Description>
-                                    </List.Content>
-                                </List.Item>
-                            )
-                        )
-                    }
-                </List.List>
-            </List.Content>
-        </List.Item>
+        <VerticalTimelineElement
+                className={`vertical-timeline-element--${props.type}`}
+                contentStyle={{ border: "1px solid #000" }}
+                contentArrowStyle={{ borderRight: '7px solid #000' }}
+                date={props.period}
+                iconStyle={{ background: '#fff', border: "0px" }}
+                icon={props.type==="education"?<MdOutlineSchool />:<MdWorkOutline />}
+            >
+            <Header as="h3" style={{margin:0}}>{props.role}
+                <Header.Subheader>{props.organization}</Header.Subheader>
+            </Header>
+
+            <p style={{padding:"10px"}}>
+                {description}
+            </p>
+        </VerticalTimelineElement>
     )
 }
 
@@ -33,12 +37,16 @@ export default function Experience() {
             <Container text textAlign="justified">
                 <Header as="h1">Experience</Header>
                 <a href='./CV.pdf'>Download CV</a>
-                <List>
-                    {
-                        experiences.map((experience, i) => <ExperienceItem key={i} {...experience} />)
-                    }
-                </List>
             </Container>
+            <VerticalTimeline lineColor="black">
+                {
+                    experiences.map(
+                        (experience, i) => (
+                            <ExperienceItem key={i} {...experience} />
+                        )
+                    )
+                }
+            </VerticalTimeline>
         </div>
     );
 }
